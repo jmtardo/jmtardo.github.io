623212ce8f9ba11846c2d8939a9311a8b70ff300
Jane <janemtardo@gmail.com>
2017-04-24 20:53:56 +0000

    * submitting project for product-project

--- projects/product-project/js/products.js
+++ projects/product-project/js/products.js
@@ -8,7 +8,16 @@ $(document).ready(function() {
 function onProductData(products) {
     //initilaize ui 
     initilaizeUI(products);
-    showProducts(products)
+    showProducts(products);
+    findMyTerm(products);
+    filterAutoPopulate(whatInDatFilter(products));
+    filterBy(products);
+    ifAnythingClicked(products);
+    lightBox(products);
+    //onProductClick(products);
+    pricesArray(products);
+    highToLow(products);
+    showProductDetails(products);
 }
 
 //create product ul 
@@ -26,8 +35,8 @@ function showProducts(products) {
     $('#products')
         .empty()
         .append(createProductListItems(products));
-        
-        //add event listerners 
+    lightBox(products);
+    //add event listerners 
 }
 
 //create all list items 
@@ -37,62 +46,227 @@ function createProductListItems(products) {
         return $('<li>') //outer box
             .addClass('li-product')
             .data('product', product)
-            //desc, price, stock, image(wrap in a div) factories 'flexbox'
-            .text(product.desc)
-            .append(createProductImageDiv(`img/product/thumbs/ ${product.img}`, 'product-thumb'))
-            .append(createProductsDetailsDiv(product.desc, product.price, product.stock));
-
+            //desc, price, stock, image(wrap in a div) factories 'FLEXBOX!'
+            //.text(product.desc)
+            .append(createProductImageDiv(`img/product/thumbs/${product.image}`, 'product-thumb'))
+            .append(createProductsDetailsDiv(product.desc, product.price, product.stock))
+            .append(createProductImageDiv(`img/product/${product.image}`, 'product-image').hide())
+            .attr("image", product.image)
+            .attr("desc", product.desc)
+            .attr("type", product.type)
+            .attr("availableColors", product.availableColors)
+            .attr("specs", product.specs)
+            .attr("price", product.price)
+            .attr("stock", product.stock);
     });
 }
 
- 
+
 function createProductImageDiv(url, cssClass) {
-  //use jquery to create and return a div that wraps an image that uses the url as its src   
+    //use jquery to create and return a div that wraps an image that uses the url as its src   
     //cssClass makes more flexible 
+    return $('<div>')
+        .append("<img src='" + url + "'>")
+        .addClass(cssClass);
 }
 
-function createProductsDetailsDiv(desc, price, stock){
-    //create div with 3 children divs, one for desc, one for price, one for stock 
-    // give each a unique class
-}
-
-
- 
-function onProductClick(event) {
-    //use on method to show product on click
-    const product = $(event.currentTarget).data('product');
-    showProductDetails(product);
-}
-
-
- 
-function showProductDetails(product) {
-//take single product, create markup for product detail & show product details 
+function createProductsDetailsDiv(desc, price, stock) {
+    return $("<div>")
+        .addClass("product-details")
+        .append($("<div>").addClass("product-description").text(desc))
+        .append($("<div>").addClass("product-price").text("Yours for only: $" + price))
+        .append($("<div>").addClass("product-stock").text("Why wait? Only " + stock + " left in stock!!"));
 
 }
 
-function search(collection, term){
+function showProductDetails(products) {
+    //take single product, create markup for product detail & show product details 
+    $("<div>")
+        .addClass("more-product-details")
+        .data("product", products);
+}
+
+function findMyTerm(products) {
+    $("#search-me-pls").on("click", function() {
+        let term = $("input.form-control").val();
+        showProducts(search(products, term));
+        lightBox(products);
+    });
+
+}
+
+//search
+function search(products, term) {
+
     // 1. create output array to collect your output []
     // 2. iterate collection .each or .reduce(hipper)   
-    
-    _.each(collection, function(value){
-    // recursive case : 
-    // iscollection, scratchpad - is array or object? 
-    //is this value a collection?
-    //IF yes, call search(value, term)
-    
-    // base case: IF is this a string?(*subsearch* / substring) & IF this string contain search term? (must be case insensitve)
-                //3.  IF yes, collect this product into your output array 
-    //recurisve function 
-    //has own array??? "match" value is pushed into array, resolves to result of calling search IF subsearch match 
-    //IF search.length 0 = falsy 
-    //value = product  (can happen many times because nested structure)
-    
-    });
-    
-     //return the output thingy (collection) array 
+    return _.reduce(products, function(previousProducts, currentProduct, i, product) {
+        if (isCollection(currentProduct)) {
+            if (search(currentProduct, term).length) {
+                previousProducts.push(currentProduct);
+            }
+        }
+
+        else if (typeof currentProduct === "string") {
+            if (currentProduct.toLowerCase().search(term.toLowerCase()) > -1) {
+                previousProducts.push(currentProduct);
+            }
+        }
+        return previousProducts;
+    }, []);
 }
+
+
+
 function isCollection(value) {
-    //is this value an array or true object 
-      // iscollection, scratchpad - is array or object? 
+    if (value === "null") {
+        return false;
+    }
+    else if (value instanceof Date) {
+        return false;
+    }
+    else if (Array.isArray(value)) {
+        return true;
+    }
+    else if (typeof value === "object") {
+        return true;
+    }
+    else if (typeof value !== "object") {
+        return false;
+    }
+}
+
+//filters
+function whatInDatFilter(products) {
+    var container = {};
+    _.reduce(products, function(filteredProds, currentProd, i, products) {
+        if (!container.hasOwnProperty(currentProd["type"])) {
+            container[currentProd["type"]] = [currentProd];
+        }
+        else if (container.hasOwnProperty(currentProd["type"])) {
+            container[currentProd["type"]].push(currentProd);
+        }
+    }, {});
+    return container;
+}
+
+function filterAutoPopulate(products) {
+    var theFilter = _.each(products, function(val, key, products) {
+        $("<li>").addClass("filtered-products").attr("id", key).text(key + "s " + " (" + val.length + ") ").prependTo(".dropdown-menu").append($("<li>").addClass("divider").appendTo(".dropdown-menu"));
+    })
+    $("<li>").addClass("prices-low-to-high").attr("id", "prices-low-to-high").text("Prices: Low to High").prependTo(".dropdown-menu").append($("<li>").addClass("divider").appendTo(".dropdown-menu"));
+    $("<li>").addClass("prices-high-to-low").attr("id", "prices-high-to-low").text("Prices: High to Low").prependTo(".dropdown-menu").append($("<li>").addClass("divider").appendTo(".dropdown-menu"));;
+    $("<li>").addClass("anything").attr("id", "any-product").text("All Products").appendTo(".dropdown-menu");
+
+}
+
+
+
+function filterBy(products) {
+    $(".filtered-products").click(function() {
+        let productType = $(event.currentTarget);
+        return showProducts(whatInDatFilter(products)[productType.attr("id")]);
+    });
+
+    $(".prices-low-to-high").click(function() {
+        let productType = $(event.currentTarget);
+        return showProducts(lowToHigh(products));
+    });
+
+    $(".prices-high-to-low").click(function() {
+        let productType = $(event.currentTarget);
+        return showProducts(highToLow(products));
+    });
+
+}
+
+
+function ifAnythingClicked(products) {
+    $(".anything").click(function() {
+        return showProducts(products);
+    });
+}
+
+
+function pricesArray(products) {
+    let output = [];
+    _.each(products, function(val, i, products) {
+        output.push(val.price)
+    });
+    output.sort(function(a, b) {
+        return a - b;
+    });
+    return _.unique(output);
+}
+
+function lowToHigh(products) {
+    let orderedArray = [];
+    _.each(pricesArray(products), function(val, pos, prices) {
+        _.each(products, function(item, i, products) {
+            //console.log("prices", val); 
+            if (item.price === prices[pos]) {
+                orderedArray.push(item);
+            }
+        });
+    });
+    return orderedArray;
+}
+
+//on click function to call this and pass it to showProducts()
+function highToLow(products) {
+    let orderedArray = [];
+    _.each(pricesArray(products).reverse(), function(val, pos, prices) {
+        _.each(products, function(item, i, products) {
+            //console.log("prices", val); 
+            if (item.price === prices[pos]) {
+                orderedArray.push(item);
+            }
+        });
+    });
+    return orderedArray;
+}
+
+
+
+//lightbox
+
+
+function lightBox(products) {
+    $('.li-product').on("click", function(event) {
+        let product = $(event.currentTarget);
+        let src = product.attr("image");
+
+
+        if (!$("#light-box").length > 0) {
+            $("body").append("<div id='light-box'>") //<img src='img/product/"  + src + "'><span class='material-icons'>CLOSE</span></div>")
+            $("<img class = 'light-box-image' src='img/product/" + src + "'>").appendTo("#light-box");
+            $("<span class='material-icons'>CLOSE</span>").appendTo("#light-box");
+            $("<div>").addClass("product-detail-text").appendTo("#light-box").css("background-color", "pink")
+                // .append("<li>").text(product.attr("image"))
+                .append($("<li>").text("Product: " + product.attr("type").toUpperCase()),
+                    $("<li>").text(product.attr("desc")),
+                    $("<li>").text("Available colors: " + product.attr("availableColors")),
+                    $("<li>").text("Specs: " + product.attr("specs")),
+                    $("<li>").text("Only  $" + product.attr("price") + "!!!"),
+                    $("<li>").text("Hurry! There are " + product.attr("stock") + " left in stock!"));
+            // $("#light-box").append("<li>").data("data", src);
+            $("#light-box").show();
+            $(".search-bar").hide();
+
+
+            $("body").on("click", "#light-box span", function() {
+                $("#light-box").remove();
+                $(".search-bar").show();
+
+            });
+        }
+        else {
+            $("#light-box").show();
+            $("body").append("<div id='light-box'> <img class = 'light-box-image' src='img/product/" + src + "'><span class='material-icons'>CLOSE</span></div>");
+            $("body").on("click", "#light-box span", function() {
+                $("#light-box").remove();
+
+            });
+        }
+    });
 }
